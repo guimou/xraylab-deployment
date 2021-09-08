cc7b708fa5f14fa0852e36010e2dd418a9569236
Guillaume Moutier <guillaume.moutier@gmail.com>
2021-09-08 11:28:30 -0400

    xraylab full

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# agnosticV
-Automated deployments configurations, to be paired with AgnosticD workloads
+# XRayLab AgnosticV
+
+Automated deployment of the XRay Lab on OpenShift.
+
+## Requirements
+
+* Order an OpenShift RHPDS environement. Last test: OpenShift 4.7 Workshop, Small.
+
+* Scale the third machineset to 1 to have 3 workers (necessary for ODF).
+
+## Deployment
+
+* Log onto the OpenShift cluster
+
+* Run `xraylab_all-in-one_deploy.sh`
--- xraylab_all-in-one_deploy.sh
+++ xraylab_all-in-one_deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-ansible-playbook -i localhost, -c local ../GPTE/agnosticd/ansible/configs/ocp-workloads/ocp-workload.yml -e @./config/xraylab_all-in-one.yaml -e ACTION=create -e guid=0001 -e ansible_user=$(whoami) -e output_dir=. -e ocp_username=opentlc-mgr
+git clone https://github.com/redhat-cop/agnosticd.git
+ansible-playbook -i localhost, -c local agnosticd/ansible/configs/ocp-workloads/ocp-workload.yml -e @./config/xraylab_all-in-one.yaml -e ACTION=create -e guid=0001 -e ansible_user=$(whoami) -e output_dir=. -e ocp_username=opentlc-mgr
